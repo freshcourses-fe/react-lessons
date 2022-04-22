505e1a1e495c8a393c28cd12d61853c89e833f85
Kirill <freshjs.ftd.@gmail.com>
2022-04-22 18:33:43 +0300

    Add Message

--- src/index.js
+++ src/index.js
@@ -2,16 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Message1(props) {
+  return (
+    <article>
+      <h2>Tom</h2>
+      <p>Toms message 1</p>
+    </article>
+  );
+}
+
+class Message extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <article
+        id={this.props.id}
+        style={{
+          padding: '10px',
+          backgroundColor: 'green',
+          width: '300px',
+          color: 'white',
+          margin: '10px',
+        }}
+      >
+        <h2>{this.props.author}</h2>
+        <p>Toms message 1</p>
+      </article>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <header>
+      <h1 id="test" className="test123">
+        Messages
+      </h1>
+    </header>
+    <main>
+      <Message id="first" author="Jerry" message="Hello there" />
+      <Message id="second" author="Tom" />
+      <Message id="third" author="Jerry" />
+    </main>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
