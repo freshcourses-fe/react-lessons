b258c6ce288f2e8a243a4ee41d2f61ed017f5ffc
Kirill <freshjs.ftd.@gmail.com>
2022-05-12 20:47:58 +0300

    Refacot Message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import React from 'react';
 import Tracker from './components/Tracker';
+import Message from './components/Message';
 
 function App() {
-  return <Tracker />;
+  return (
+    <>
+      <Message isImportant />
+      <Message />
+    </>
+  );
 }
 
 export default App;
--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,47 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
-class Message extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      isRead: false,
-    };
-  }
-  toggleIsRead = () => {
-    this.setState({
-      isRead: !this.state.isRead,
-    });
+function Message({ id, author = 'Author', message = 'message', isImportant }) {
+  const [isRead, setIsRead] = useState(false);
+
+  const toggleIsRead = () => {
+    setIsRead(!isRead);
   };
 
-  render() {
-    const {
-      id,
-      author = 'Author',
-      message = 'message',
-      isImportant = false,
-    } = this.props;
+  const styles = {
+    padding: '10px',
+    backgroundColor: isImportant ? 'violet' : 'green',
+    width: '300px',
+    color: 'white',
+    margin: '10px',
+  };
 
-    const { isRead } = this.state;
-
-    const styles = {
-      padding: '10px',
-      backgroundColor: isImportant ? 'violet' : 'green',
-      width: '300px',
-      color: 'white',
-      margin: '10px',
-    };
-
-    return (
-      <article style={styles} onClick={this.toggleIsRead}>
-        {isImportant && <strong>IMPORTANT MESSAGE</strong>}
-        <h2>{author}</h2>
-        <p>{message}</p>
-        {isRead && <small>ПРОЧИТАНО</small>}
-        
-      </article>
-    );
-  }
+  return (
+    <article style={styles} onClick={toggleIsRead}>
+      {isImportant && <strong>IMPORTANT MESSAGE</strong>}
+      <h2>{author}</h2>
+      <p>{message}</p>
+      {isRead && <small>ПРОЧИТАНО</small>}
+    </article>
+  );
 }
 
 export default Message;
