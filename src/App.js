3ba77d8e1bc336e1fd368fd3cc224c7229870994
Kirill <freshjs.ftd.@gmail.com>
2022-05-13 19:15:24 +0300

    Add useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Tracker from './components/Tracker';
 import Message from './components/Message';
 
 function App() {
+  const [isMessagesVisible, setIsMessagesVisible] = useState(true);
+
+  const toggleMessageVisibility = () => {
+    setIsMessagesVisible(!isMessagesVisible);
+  };
+
   return (
     <>
-      <Message isImportant />
-      <Message />
+      <button onClick={toggleMessageVisibility}>Toggle Messages</button>
+      {isMessagesVisible && (
+        <div>
+          <Message isImportant />
+          <Message />
+          <Tracker />
+        </div>
+      )}
     </>
   );
 }
--- src/components/Tracker/index.jsx
+++ src/components/Tracker/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Tracker = (props) => {
   const [clicks, setClicks] = useState(0);
@@ -7,10 +7,6 @@ const Tracker = (props) => {
     y: 0,
   });
 
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
   const handleMouseMove = (event) => {
     setCoordinates({
       x: event.clientX,
@@ -18,12 +14,31 @@ const Tracker = (props) => {
     });
   };
 
+  // useEffect(function componentDidMountAndUpdate() {
+  //   console.log('render');
+  //   document.body.addEventListener('mousemove', handleMouseMove);
+
+  //   return function componentWillUnmount() {
+  //     console.log('unmount');
+  //     document.body.removeEventListener('mousemove', handleMouseMove);
+  //   };
+  // }, []);
+
+  const handleClick = () => {
+    setClicks((prevClick) => prevClick + 1);
+  };
+  useEffect(() => {
+    document.body.addEventListener('click', handleClick);
+
+    return () => {
+      document.body.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  console.log('render');
+
   return (
-    <div
-      style={{ background: 'gray', minHeight: '100vh' }}
-      onClick={handleClick}
-      onMouseMove={handleMouseMove}
-    >
+    <div style={{ background: 'gray', minHeight: '100vh' }}>
       <p>clicks: {clicks}</p>
       <p>X: {coordinates.x}</p>
       <p>Y: {coordinates.y}</p>
