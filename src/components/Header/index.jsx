c02df9e0c77b2c32dfaf0053caad10d3f8577a36
Kirill <freshjs.ftd.@gmail.com>
2022-05-16 20:48:41 +0300

    Add useCallback

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,23 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState, useCallback } from 'react';
 import { ThemeContext, UserContext } from '../../contexts';
 import './Header.scss';
 
 function Header(props) {
   const [theme, toggleTheme] = useContext(ThemeContext);
   const [user, setUser] = useContext(UserContext);
+  const [inputValue, setValue] = useState('');
+
+  const handleChange = useCallback(({ target: { value } }) => {
+    setValue(value);
+  }, []);
+
+  const logUser = useCallback(() => {
+    console.log(`${user.name} ${user.lastName}`);
+  }, [user]);
+
+  useEffect(() => {
+    console.log('render');
+  }, [logUser]);
 
   return (
     <header className={theme}>
@@ -12,6 +25,8 @@ function Header(props) {
         {user.name} {user.lastName}
       </h1>
       <button onClick={toggleTheme}>theme</button>
+      <button onClick={logUser}>Click me</button>
+      <input value={inputValue} onChange={handleChange} />
     </header>
   );
 }
