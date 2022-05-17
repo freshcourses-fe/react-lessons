c9d5bcccdef64ae5574ac75e80af88284b568477
Kirill <freshjs.ftd.@gmail.com>
2022-05-17 18:22:23 +0300

    Add useMemo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,16 @@
-import { useContext, useEffect, useState, useCallback } from 'react';
+import { useContext, useEffect, useState, useCallback, useMemo } from 'react';
 import { ThemeContext, UserContext } from '../../contexts';
 import './Header.scss';
+
+function heavyCalcFunction(inputValue) {
+  let x = 0;
+
+  for (let i = 0; i < 1_000_000_000; i++) {
+    x++;
+  }
+
+  return inputValue ** 9;
+}
 
 function Header(props) {
   const [theme, toggleTheme] = useContext(ThemeContext);
@@ -8,7 +18,7 @@ function Header(props) {
   const [inputValue, setValue] = useState('');
 
   const handleChange = useCallback(({ target: { value } }) => {
-    setValue(value);
+    setValue(Number(value));
   }, []);
 
   const logUser = useCallback(() => {
@@ -19,14 +29,20 @@ function Header(props) {
     console.log('render');
   }, [logUser]);
 
+  const calculatedValue = useMemo(
+    () => heavyCalcFunction(inputValue),
+    [inputValue]
+  );
+
   return (
     <header className={theme}>
       <h1>
         {user.name} {user.lastName}
       </h1>
+      <h2>{calculatedValue}</h2>
       <button onClick={toggleTheme}>theme</button>
       <button onClick={logUser}>Click me</button>
-      <input value={inputValue} onChange={handleChange} />
+      <input type="number" value={inputValue} onChange={handleChange} />
     </header>
   );
 }
