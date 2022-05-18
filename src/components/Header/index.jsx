5a90e75989be4ab6a4c606cec8bdfe62b8675d17
Kirill <freshjs.ftd.@gmail.com>
2022-05-18 19:21:23 +0300

    Refactor Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,48 +1,39 @@
-import { useContext, useEffect, useState, useCallback, useMemo } from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { ThemeContext, UserContext } from '../../contexts';
 import './Header.scss';
-
-function heavyCalcFunction(inputValue) {
-  let x = 0;
-
-  for (let i = 0; i < 1_000_000_000; i++) {
-    x++;
-  }
-
-  return inputValue ** 9;
-}
 
 function Header(props) {
   const [theme, toggleTheme] = useContext(ThemeContext);
   const [user, setUser] = useContext(UserContext);
-  const [inputValue, setValue] = useState('');
-
-  const handleChange = useCallback(({ target: { value } }) => {
-    setValue(Number(value));
-  }, []);
-
-  const logUser = useCallback(() => {
-    console.log(`${user.name} ${user.lastName}`);
-  }, [user]);
-
-  useEffect(() => {
-    console.log('render');
-  }, [logUser]);
-
-  const calculatedValue = useMemo(
-    () => heavyCalcFunction(inputValue),
-    [inputValue]
-  );
 
   return (
     <header className={theme}>
-      <h1>
-        {user.name} {user.lastName}
-      </h1>
-      <h2>{calculatedValue}</h2>
-      <button onClick={toggleTheme}>theme</button>
-      <button onClick={logUser}>Click me</button>
-      <input type="number" value={inputValue} onChange={handleChange} />
+      <div className="headerContainer">
+        <h1>
+          Hello {user.name} {user.lastName}
+        </h1>
+        <button onClick={toggleTheme}>theme</button>
+      </div>
+      <nav>
+        <ul className='navList'>
+          <li>
+            <Link className='navLink' to="/">Home</Link>
+          </li>
+          <li>
+            <Link className='navLink' to="/signin">Sign-in</Link>
+          </li>
+          <li>
+            <Link className='navLink' to="/signup">Sign-up</Link>
+          </li>
+          <li>
+            <Link className='navLink' to="/laptops">Laptops</Link>
+          </li>
+          <li>
+            <Link className='navLink' to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
